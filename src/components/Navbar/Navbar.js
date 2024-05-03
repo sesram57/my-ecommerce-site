3ba70333d255c4a444139abd8600c381d9a2ff26
Olivier Morhain <morhain.olivier0454@orange.fr>
2024-05-03 14:47:33 +0200

    Adding Router and modify Navbar for use it

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,13 @@
+import { Link } from 'react-router-dom'
+
 function Navbar(props) {
-  const PageList = props.pageList;
-  const lastIndex = PageList.length;
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-teal-600 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl tracking-tight">
-          My Ecommerce Site
-        </span>
-      </div>
-      <div className="inline-block flex-grow items-center ">
-        <div className="items-center text-sm flex-grow">
-          {PageList.map((item) => (
-            <a
-              key={item.id}
-              href={item.adress}
-              className={`mt-2 inline-block lg:mt-0 text-teal-100 hover:text-white ${lastIndex === item.id ? "" : "mr-4"}`}
-            >
-              {item.name}
-            </a>
-          ))}
-        </div>
-      </div>
+    <nav>
+      
+             <Link to="/" className='mr-4'>Accueil</Link>
+             <Link to="/categories" className='mr-4'>Catégories</Link>
+             <Link to="/produits" className='mr-4'>Produits</Link>
+             <Link to="/contact">Contact</Link>
     </nav>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './styles/index.css';
-import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
+import Accueil from './pages/Accueil/Accueil';
+import Categories from './pages/Categories/Categories';
+import Contact from './pages/Contact/Contact';
+import Produits from './pages/Produits/Produits';
+import Navbar from './components/Navbar/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Navbar />
+      <Routes>
+          <Route path='/' element={<Accueil />} />
+          <Route path='/categories' element={<Categories />} />
+          <Route path='/produits' element={<Produits />} />
+          <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
